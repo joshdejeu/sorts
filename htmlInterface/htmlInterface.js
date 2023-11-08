d75ce3c22ea3b6770c529c1998e6ca19afe94a57
John Smith <joshplays21@gmail.com>
2023-11-08 14:16:38 -0600

    removed console logs

--- htmlInterface/htmlInterface.js
+++ htmlInterface/htmlInterface.js
@@ -190,7 +190,6 @@ export class HTMLInterface {
 
             if(soundVolume === null)soundVolume = 25;
             const clampedGain = Math.max(0, Math.min(1, soundVolume / 100)).toFixed(2);
-            console.log(soundVolume, clampedGain)
             gainNode.gain.value = clampedGain;
 
             source.start(0);

d3fa11cb4438c7088c3c89a5a850070dddc96f30
joshdejeu <dejeujosh@gmail.com>
2023-11-11 01:16:52 -0600

    closest merge sort

--- sorts/merge.js
+++ sorts/merge.js
@@ -4,77 +4,89 @@ import { sound, sort_speed } from '../script.js'
 export async function theMergeSort(arrayToSort, upperBoundBarVal, onCompleteCallback) {
     var container = document.getElementById("container");
     var children = container.children;
-    console.log(arrayToSort)
-    await mergeSort(arrayToSort);
-    console.log(arrayToSort)
-    onCompleteCallback()
 
 
-    async function mergeSort(arr) {
-        const n = arr.length;
-        const temp = new Array(n);
+    const mergeSort = async (arr) =>  {
+        //Create two arrays for sorting
+        let sorted = Array.from(arr);
+        let n = sorted.length;
+        let buffer = new Array(n);
 
         for (let size = 1; size < n; size *= 2) {
-            for (let leftStart = 0; leftStart < n - 1; leftStart += 2 * size) {
-                const leftEnd = Math.min(leftStart + size - 1, n - 1);
-                const rightStart = leftEnd + 1;
-                const rightEnd = Math.min(leftStart + 2 * size - 1, n - 1);
+            for (let leftStart = 0; leftStart < n; leftStart += 2 * size) {
 
-                // Visualize the merge by swapping elements
-                // await swap(children[leftStart], children[rightStart], 100); // Adjust the time delay as needed
-                merge(arr, temp, leftStart, leftEnd, rightStart, rightEnd);
+                //Get the two sub arrays
+                let left = leftStart,
+                    right = Math.min(left + size, n),
+                    leftLimit = right,
+                    rightLimit = Math.min(right + size, n);
+
+                //Merge the sub arrays
+                await merge(left, right, leftLimit, rightLimit, sorted, buffer);
             }
+
+            //Swap the sorted sub array and merge them
+            let temp = sorted;
+            sorted = buffer;
+            buffer = temp;
         }
 
-        return arr;
+        return sorted;
     }
 
-    function merge(arr, temp, leftStart, leftEnd, rightStart, rightEnd) {
-        let i = leftStart;
-        let j = rightStart;
-        let k = leftStart;
+    const merge = async (left, right, leftLimit, rightLimit, sorted, buffer) => {
+        let i = left;
 
-        while (i <= leftEnd && j <= rightEnd) {
-            if (arr[i] <= arr[j]) {
-                temp[k++] = arr[i++];
+        //Compare the two sub arrays and merge them in the sorted order
+        while (left < leftLimit && right < rightLimit) {
+            if (sorted[left] <= sorted[right]) {
+                buffer[i++] = sorted[left++];
             } else {
-                temp[k++] = arr[j++];
+                await swap(children[i], children[right], sort_speed);
+                buffer[i++] = sorted[right++];
             }
         }
 
-        while (i <= leftEnd) {
-            temp[k++] = arr[i++];
+        //If there are elements in the left sub arrray then add it to the result
+        while (left < leftLimit) {
+            buffer[i++] = sorted[left++];
         }
 
-        while (j <= rightEnd) {
-            temp[k++] = arr[j++];
-        }
-
-        for (let x = leftStart; x <= rightEnd; x++) {
-            arr[x] = temp[x];
+        //If there are elements in the right sub array then add it to the result
+        while (right < rightLimit) {
+            buffer[i++] = sorted[right++];
         }
     }
 
+
+
+    
+    console.log(arrayToSort)
+
+    arrayToSort = await mergeSort(arrayToSort, Array.from(children));
+
+    console.log(arrayToSort)
+    onCompleteCallback()
 }
 
 
-async function swap(element1, element2, time) {
+
+async function swap(element1, element2, sort_speed) {
     return new Promise((resolve) => {
-      const parent = element1.parentNode;
-      const temp = document.createElement('div'); // Create a temporary element
-      parent.insertBefore(temp, element1);
-      parent.insertBefore(element1, element2.nextSibling); // Use element2.nextSibling to correctly insert after element2
-      parent.insertBefore(element2, temp);
-      parent.removeChild(temp);
-  
-      // Resolve the Promise after a minimum of `time` milliseconds
-      if (time !== 0) {
-        setTimeout(() => {
-          resolve();
-        }, time);
-      } else {
-        resolve();
-      }
+        const parent = element1.parentNode;
+        const temp = document.createElement('div'); // Create a temporary element
+        parent.insertBefore(temp, element1);
+        parent.insertBefore(element1, element2.nextSibling); // Use element2.nextSibling to correctly insert after element2
+        parent.insertBefore(element2, temp);
+        parent.removeChild(temp);
+
+        // Resolve the Promise after a minimum of `sort_speed` milliseconds
+        if (sort_speed !== 0) {
+            setTimeout(() => {
+                resolve();
+            }, sort_speed);
+        } else {
+            resolve();
+        }
     });
-  }
-  
+}
